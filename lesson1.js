4b552c02d7dda97d80872e8c8066875d0703eb38
Mahdi <moshiurrahmanmehedi210@gmail.com>
2026-06-24 09:49:29 +0600

    Variables (let, const) & Data Types (Backend Use-Case)

--- lesson1.js
+++ lesson1.js
@@ -1,28 +1,40 @@
-const TAX_RATE= 0.15;
-const SHIPING_COST= 5.00;
+const DATABASE_URL = "mongodb://localhost:27017/my_app";
+console.log("Database URL:", DATABASE_URL);
 
-const incomingCartItem={
-    id: "product-1",
-    name:"Wireless Mouse",
-    price: 25.00,
-    quantity: 2
+// DATABASE_URL ="mongodb://localhost:27017/another_app"; 
+
+
+let cartTotal = 0;
+const item1Price = 250;
+const item2Price = 150;
+
+cartTotal= item1Price + item2Price;
+console.log("Cart Total:", cartTotal);
+
+//Backend object and data types(example: Api response)
+
+const userProfile={
+    userId: 1024,                   // Number
+    username: "codeMAHDI",          // String
+    email: "mahdiappdev@gmail.com", // String
+    isActive: true,                 // Boolean
+    roles: ["developer", "admin"]   // Array
 };
 
-const processOrder= (item)=>{
-    subtotal= item.price*item.quantity;
-    const taxAmount= subtotal*TAX_RATE;
-    const totatAmount= subtotal+taxAmount+SHIPING_COST;
+console.log("User profile object:", userProfile);
+console.log("Is User Admin?", userProfile.roles.includes("admin"));
 
-    return{
-        productName: item.name,
-        subtotal: subtotal,
-        tax: taxAmount,
-        shipping: SHIPING_COST,
-        finalTotal: totatAmount,
-        isFreeShipping: totatAmount>50
-    };
 
+
+// Ecommerce product object
+
+const productDetails={
+    productId: 1,
+    title: "Headphones",
+    price: 99.99,
+    isAvailable: true,
+    tags:["electronics", "audio", "accessory"]
 };
 
-const orderSummary= processOrder(incomingCartItem);
-console.log(orderSummary);
+console.log("Product Details:", productDetails);
+console.log("Price of the product is:", productDetails.price);
