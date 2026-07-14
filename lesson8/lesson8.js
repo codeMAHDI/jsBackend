de51679b0a938330e1960f21db8e7926f1a46ed1
codeMAHDI <moshiurrahmanmehedi210@gmail.com>
2026-07-14 16:11:00 +0600

    Event loop initial understanding completed

---
+++ lesson8/lesson8.js
@@ -0,0 +1,7 @@
+// Event loop 
+// Event loop is a mechanism that allows JavaScript to 
+// perform non-blocking operations by offloading 
+// operations to the system kernel whenever possible. 
+// It is responsible for executing the code, collecting and processing events, 
+// and executing queued sub-tasks. Cause Js is single-threaded, 
+// it can only do one thing at a time.
