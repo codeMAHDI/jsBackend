d73d250c3329eb37ad891a9774ee7f578424e943
codeMAHDI <moshiurrahmanmehedi210@gmail.com>
2026-07-16 12:45:55 +0600

    Completed the base structure for backend server

---
+++ Phase-2/lesson2.js
@@ -0,0 +1,5 @@
+//Dependencies Install & Server Run 
+// Express.js is a web application framework for Node.js. 
+// It provides a set of features and tools for building web applications and APIs.
+//Nodemon is a utility that monitors for any changes in your 
+// source and automatically restarts your server.
