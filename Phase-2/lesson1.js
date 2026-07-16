acc110f21a1b5e20b13f67b4e7b2e0902f4b916d
codeMAHDI <moshiurrahmanmehedi210@gmail.com>
2026-07-16 12:14:35 +0600

    Phase 2, lesson 1 completed

---
+++ Phase-2/lesson1.js
@@ -0,0 +1,7 @@
+// Node.js
+// NPM - Node Package Manager
+// package.json - A file that contains metadata about the project and its dependencies. 
+// A package.json file is created when you run the command npm init in your project directory. 
+// It contains information such as the project name, version, description, author, license, and dependencies. 
+// The dependencies section lists the packages that your project depends on, along with their versions. 
+// You can install packages using npm install <package-name> and they will be added to the dependencies section of your package.json file.
